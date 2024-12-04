8f7f1379ce829c58c13dbd76f9a78a00b2fbeab6
HI-CCUP Hiccup <maciejkarolzawada@gmail.com>
2024-12-04 16:52:45 +0000

    frontend update 1

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,18 +1,30 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let blogs = ref([]);
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const title= target.querySelector('#title').value;
-  const content= target.querySelector('#tcontent').value;
-  const tags= target.querySelector('#tags').value;
+  const title = target.querySelector('#title').value;
+  const content = target.querySelector('#content').value;
+  const tags = target.querySelector('#tags').value;
   const splitedTags = tags.split(",")
 
-  await my_project_backend.add_blog(title, content, splitedTags);
+  await my_project_backend.add_blog(title, content, splitedTags)
+  await getBlogs();
 }
+
+async function getBlogs(){
+  const tempBlogs = await my_project_backend.get_blogs();
+  blogs.value = tempBlogs.map((blog) => {
+    return {
+      ...blog,
+      date: blog.date.toString()
+    }
+  })
+}
+getBlogs();
 </script>
 
 <template>
@@ -20,7 +32,7 @@ async function handleSubmit(e) {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <form action="#" @submit="handleSubmit">  
+    <form action="#" @submit="handleSubmit">
       <div><p>Title: </p><input id="title" alt="title" type="text" /></div>
 
       <div><p>Content: </p><input id="content" alt="content" type="text" /></div>
@@ -28,5 +40,6 @@ async function handleSubmit(e) {
       <div><p>Tags: </p><input id="tags" alt="tags" type="text" /></div>
       <button type="submit">Click to add!</button>
     </form>
+    {{ blogs }}
   </main>
 </template>
